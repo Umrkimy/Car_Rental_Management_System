4833a390ee30b28d8a28748012be692f49ef1f00
Umrkimy <MGeek122@yahoo.com>
2024-11-02 03:42:53 +0800

    comment smtp so it wont cause any problem

--- emailSend.php
+++ emailSend.php
@@ -13,7 +13,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->isSMTP();                                            //Send using SMTP
+    //$mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'mgeek573@gmail.com';                     //SMTP username
