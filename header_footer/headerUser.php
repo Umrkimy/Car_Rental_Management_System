26fc47131c0f985daf8697a56248dbbcedd6f798
Umrkimy <MGeek122@yahoo.com>
2024-11-02 03:29:24 +0800

    first commit

---
+++ header_footer/headerUser.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<head >
+    <title></title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="css/customstylesheet.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand me-3 fw-bold fs-3 h-font" href="index.php">
+                <img src="imgs/logo.jpg" width="30" height="30" />
+                Car Rental
+            </a>
+
+            <div class="collapse navbar-collapse mr-auto" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active me-2 fw-bold fs-6">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item active me-2 fw-bold fs-6">
+                        <a class="nav-link" href="aboutUs.php">About Us</a>
+                    </li>
+                    <li class="nav-item active me-2 fw-bold fs-6">
+                        <a class="nav-link" href="feedback.php">Feedback</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active fw-bold fs-6">
+                        <a class="nav-link" href="userLogin.php">Login</a>
+                       <!-- <a class="nav-link" href="homepage.php">Logout</a>-->
+                    </li>
+
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</body>
---
+++ userHomepage.php
@@ -0,0 +1,97 @@
+<?php
+include("header_footer/headerUser.php");
+?>
+
+<section>
+    <img src="imgs/facilities.jpg" class="img-fluid" />
+</section>
+
+<style>
+    .rooms{
+        background: whitesmoke
+    }
+
+    .facilities{
+        background: whitesmoke
+    }
+</style>
+
+<section class="rooms">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-2">
+                <center>
+                    <h1>Our Rooms</h1>
+                    <p><b>Explore our comfortable accommodation options:</b></p>
+                </center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <center>
+                    <img src="imgs/deluxe.jpg" style="max-width: 100%; height: auto;" />
+                    <h4>Deluxe Room</h4>
+                    <p class="text-justify">Enjoy luxurious comfort in our deluxe rooms, perfect for a relaxing stay.</p>
+                </center>
+            </div>
+            <div class="col-md-4">
+                <center>
+                    <img src="imgs/family.jpg" style="max-width: 100%; height: auto;" />
+                    <h4>Family Room</h4>
+                    <p class="text-justify">Spacious and accommodating, our family rooms provide a comfortable retreat for your loved ones.</p>
+                </center>
+            </div>
+            <div class="col-md-4">
+                <center>
+                    <img src="imgs/single.jpg" style="max-width: 100%; height: auto;" />
+                    <h4>Single Room</h4>
+                    <p class="text-justify">Experience comfort and convenience in our cozy single rooms, ideal for solo travelers.</p>
+                </center>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <img src="imgs/facilit.jpg" class="img-fluid" />
+</section>
+
+<section class="facilities">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-2">
+                <center>
+                    <h1>Our Facilities</h1>
+                    <p><b>Discover our top-notch amenities:</b></p>
+                </center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <center>
+                    <img src="imgs/pools.jpg" style="max-width: 100%; height: auto;" />
+                    <h4>Pool</h4>
+                    <p class="text-justify">Take a refreshing dip in our sparkling pool, the perfect escape from the heat.</p>
+                </center>
+            </div>
+            <div class="col-md-4">
+                <center>
+                    <img src="imgs/gym.png" style="max-width: 100%; height: auto;" />
+                    <h4>Gym</h4>
+                    <p class="text-justify">Stay active and energized with our state-of-the-art fitness facilities, equipped for all your workout needs.</p>
+                </center>
+            </div>
+            <div class="col-md-4">
+                <center>
+                    <img src="imgs/hotel.jpg" style="max-width: 100%; height: auto;" />
+                    <h4>Restaurant</h4>
+                    <p class="text-justify">Indulge in exquisite culinary delights at our restaurant, offering a diverse menu to satisfy every palate.</p>
+                </center>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include("header_footer/footerUser.php");
+?>
