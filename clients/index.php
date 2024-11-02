2ee5f01d9992a26a216f82dc2b24819b4fd81c12
Umrkimy <MGeek122@yahoo.com>
2024-11-03 00:59:26 +0800

    Revert "make the css work"
    This reverts commit bae38e080f80f6c09caf7aa192550bcaad8a1a8f.

--- clients/index.php
+++ clients/index.php
@@ -1,10 +1,9 @@
 <?php
 $title = 'Clients Home';
-require_once("../includes/headerUser.php");
+require_once("includes/headerUser.php");
 ?>
 
-<link href="../css/customstylesheet.css" rel="stylesheet" />
 
 <?php
-require_once("../includes/footerUser.php");
+require_once("includes/footerUser.php");
 ?>
